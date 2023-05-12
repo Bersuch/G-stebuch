995d796e07773efdfb7f73b272465fb2e7e7ca29
Bersuch <bersuchdaniel@yahoo.de>
2023-05-12 09:18:33 +0200

    changed minor migration issues

--- database/migrations/2023_03_13_112408_create_guest_book_entries_table.php
+++ database/migrations/2023_03_13_112408_create_guest_book_entries_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('subtitle', 255);
             $table->text('body');
-            $table->id('user_id');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
         });
